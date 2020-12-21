5f91d1482ccbf3df2806c35b634c591ece665527
2rist-by <76200787+2rist-by@users.noreply.github.com>
2020-12-21 14:21:08 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -22,8 +22,8 @@ $(document).ready(function(){
 	  			      breakpoint: 1024,
 	  			      settings: {
 	  			        slidesToShow: 1,
-	  			        prevArrow: '<button class="prev arrow"></button>',
-	  			        nextArrow: '<button class="next arrow"></button>',
+	  			        prevArrow: '<button class="prev arrow feedback_arrow"></button>',
+	  			        nextArrow: '<button class="next arrow feedback_arrow"></button>',
 	  			        slidesToScroll: 1
 	  			      }
 	  			    }	  			    
@@ -48,17 +48,17 @@ $(document).ready(function(){
 	  					    },
 
 	  					    {
-	  					      breakpoint: 600,
+	  					      breakpoint: 690,
 	  					      settings: {
 	  					        slidesToShow: 2,
 	  					        prevArrow: '<button class="prev arrow"></button>',
-	  					        nextArrow: '<button class="next arrow"></button>',
+	  					        nextArrow: '<button class="next arrow" ></button>',
 	  					        slidesToScroll: 2
 	  					      }
 	  					    },
 
 	  					    {
-	  					      breakpoint: 450,
+	  					      breakpoint: 465,
 	  					      settings: {
 	  					        slidesToShow: 1,
 	  					        prevArrow: '<button class="prev arrow"></button>',
